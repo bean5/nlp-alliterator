7465beff7b4e4cf8a1739001f2ed1ce622471e07
sampwing <sampwing@gmail.com>
2012-03-18 02:38:42 -0700

    really bad

--- alliterator.py
+++ alliterator.py
@@ -1,4 +1,28 @@
+from nltk.corpus import wordnet
+
+def alliterate(word, character):
+   if word[0] == character:
+      return word
+   results = [query.name for query in wordnet.synsets(word)]
+   for result in results:
+      try:
+         if result[0] == character:
+            return result[:result.find('.')]
+      except IndexError, e:
+         pass
+   return word
+
 def main():
-   pass
+   sentence = ''
+   character = 's'
+   while True:
+      if 'exit' in sentence:
+         break
+      sentence = raw_input('Type a sentence: ')
+      tokens = sentence.split(' ')
+      results = [alliterate(word=token, character=character) for token in tokens]
+      print ' '.join(results)
+      
+
 if __name__ == '__main__':
    main()
